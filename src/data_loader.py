3f5839f41555cf6a93a8db7fada0de63273595ec
shigenogoro <kyle990987@gmail.com>
2025-11-27 02:01:29 -0500

    fix data_loader

--- src/data_loader.py
+++ src/data_loader.py
@@ -5,7 +5,14 @@ def load_meetingbank():
     Load the MeetingBank dataset from HuggingFace.
     Returns:
         train_data: Dataset
+        val_data: Dataset
         test_data: Dataset
     """
-    dataset = load_dataset("MeetingBank-HF/MeetingBank")
-    return dataset["train"], dataset["test"]
+    meetingbank = load_dataset("huuuyeah/meetingbank")
+
+    train_data = meetingbank['train']
+    test_data = meetingbank['test']
+    val_data = meetingbank['validation']
+
+    return train_data, val_data, test_data
+
