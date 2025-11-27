c9613417de934678b0b4ad15537d445d5d0957db
shigenogoro <kyle990987@gmail.com>
2025-11-27 03:25:36 -0500

    preprocessing

--- src/utils.py
+++
@@ -1,6 +0,0 @@
-def data_generator(dataset):
-    """
-    Create a generator that yields (id, summary, transcript).
-    """
-    for instance in dataset:
-        yield instance["id"], instance["summary"], instance["transcript"]
